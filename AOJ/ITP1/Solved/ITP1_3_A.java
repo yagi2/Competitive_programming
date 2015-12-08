65a1338d13076060a8922e16621c94c04d22ac09
Itsuki Aoyagi <mail@yagi2.com>
2015-12-08 10:32:28 +0900

    [Java]ITP1_3_A Solved

---
+++ AOJ/ITP1/Solved/ITP1_3_A.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+	for (int i = 0; i < 1000; i++) {
+	    System.out.println("Hello World");
+	}
+    }
+}
